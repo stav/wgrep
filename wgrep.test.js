32e820a2b2e3054d722af8104eb7cce30ce51c2c
Steven Almeroth <sroth77@gmail.com>
2020-03-04 12:09:25 -0500

    add suites

--- wgrep.test.js
+++ wgrep.test.js
@@ -3,20 +3,24 @@
  */
 const wgrep = require('./wgrep');
 
-test('find text in folder', () => {
-  const dir = '.github';
-  const regex = /actions/;
-  const files = wgrep.find( dir, regex )
-  expect(files).toHaveLength(1);
+describe('Find', () => {
+  test('should find text in folder', () => {
+    const dir = '.github';
+    const regex = /actions/;
+    const files = wgrep.find( dir, regex )
+    expect(files).toHaveLength(1);
+  });
+
+  test('should find nothing in non-existent folder', () => {
+    const dir = 'DOES-NOT-EXIST';
+    const regex = /.+/;
+    const files = wgrep.find( dir, regex )
+    expect(files).toHaveLength(0);
+  });
 });
 
-test('find nothing in non-existent folder', () => {
-  const dir = 'DOES-NOT-EXIST';
-  const regex = /.+/;
-  const files = wgrep.find( dir, regex )
-  expect(files).toHaveLength(0);
-});
-
-test('show returns undefined', () => {
-  expect(wgrep.show([])).toBeUndefined();
+describe('Show', () => {
+  test('should show returns undefined', () => {
+    expect(wgrep.show([])).toBeUndefined();
+  });
 });
