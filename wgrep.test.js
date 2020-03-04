e2b9b1e4f050e6e4a413c9ffa432605ac1f385ad
Steven Almeroth <sroth77@gmail.com>
2020-03-04 13:38:26 -0500

    download tests

--- wgrep.test.js
+++ wgrep.test.js
@@ -3,6 +3,26 @@
  */
 const wgrep = require('./wgrep');
 
+describe('Download', () => {
+  test('should return no errors', async () => {
+    const download = await wgrep.download('https://example.com/', 'output');
+    expect(download).toStrictEqual(expect.objectContaining({flag: false}));
+  });
+
+  test('should return network error', async () => {
+    const download = await wgrep.download('badscheme://example.com/', 'output');
+    expect(download).toStrictEqual(expect.objectContaining({net: 1}));
+  });
+
+  // // This will increase code coverage to 100% if we hardcode the parent
+  // // traversal correctly but kinda lame, dangerous and hard to cleanup.
+  // // Need to setup/teardown a target directory without write permissions
+  // test('should return buffer error', async () => {
+  //   const download = await wgrep.download('https://example.com/', '../../../../');
+  //   expect(download).toStrictEqual(expect.objectContaining({buf: 1}));
+  // });
+});
+
 describe('Find', () => {
   test('should find text in folder', () => {
     const dir = '.github';
@@ -21,6 +41,6 @@ describe('Find', () => {
 
 describe('Show', () => {
   test('should show returns undefined', () => {
-    expect(wgrep.show([])).toBeUndefined();
+    expect(wgrep.show([null])).toBeUndefined();
   });
 });
