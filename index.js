bda1c018633871435fb595be3abae4d989147ad4
Steven Almeroth <sroth77@gmail.com>
2021-02-07 13:26:26 -0500

    update deps

--- index.js
+++ index.js
@@ -14,13 +14,15 @@ const program = require('commander');
     .option('-u, --username <username>', 'The user to authenticate as')
     .action(function( regex, url ) {
 
+      const options = program.opts()
+
       co(async function () {
-        console.log(`Calling for "${regex}" in "${program.directory}" from "${url}" with user "${program.username}"`)
-        const errors = await wgrep.download( url, program.directory );
+        console.log(`Calling for "${regex}" in "${options.directory}" from "${url}" with user "${options.username}"`)
+        const errors = await wgrep.download( url, options.directory );
         if ( errors.flag ) {
           console.log('Errors', errors)
         }
-        wgrep.show(wgrep.find( program.directory, regex ))
+        wgrep.show(wgrep.find( options.directory, regex ))
       });
 
    })
